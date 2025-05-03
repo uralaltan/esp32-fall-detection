fd657d14ea3e1dd9e0e7c7266ea886176a6986c9
Altan <uralaltan10@gmail.com>
2025-05-03 20:01:22 +0300

    Added MPU6500 and read 6 axis data

--- main/esp32-fall-detection.c
+++ main/esp32-fall-detection.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include "imu.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-void app_main(void)
-{
+static void imu_task(void *arg) {
+    for (;;) {
+        imu_read_and_print();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
+void app_main(void) {
+    imu_init();
+    xTaskCreate(imu_task, "imu", 2048, NULL, 5, NULL);
 }
